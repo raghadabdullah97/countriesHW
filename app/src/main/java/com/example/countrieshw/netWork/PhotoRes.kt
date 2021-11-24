646e1fb0ac6800fe68f1ed95041e5fa36f68dc24
Raghad Abdullah <Raghad.Abdullah.ruh.k@tuwaiq.edu.sa>
2021-11-24 10:21:09 +0300

    .

--- app/src/main/java/com/example/countrieshw/netWork/PhotoRes.kt
+++ app/src/main/java/com/example/countrieshw/netWork/PhotoRes.kt
@@ -1,2 +1,8 @@
 package com.example.countrieshw.netWork
 
+import android.provider.ContactsContract
+import com.squareup.moshi.Json
+
+data class PhotoRes (val error : Boolean , val msg : String , val data : List<Photo>)
+data class photo (@Json(name = "name") val name : String ,
+                  @Json(name = "flag") val flagUrl : String)
